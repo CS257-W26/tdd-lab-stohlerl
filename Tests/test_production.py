1223f9a743f7e660b7f1118ecd9a5094d612780b
Lloyd Stohler <lstohler22@tjs.org>
2026-01-16 09:28:44 -0600

    upd8ed

--- Tests/test_production.py
+++ Tests/test_production.py
@@ -1,9 +1,21 @@
 '''
 A file for tests for the production code.
 '''
-
+import unittest
 from production import reverse_word
+
+from test_production import *
 
 class TestReverseWord(unittest.TestCase):
 
-  pass
+  def test_reverse_olleh(self):
+    self.assertEqual(reverse_word("olleh"), "hello")
+
+  def test_reverse_na(self):
+    self.assertEqual(reverse_word(""), "")
+
+  def test_reverse_numbers(self):
+    self.assertEqual(reverse_word("12345"), "54321")
+
+
+
